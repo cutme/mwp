873deebb07d4669e261323d396912f60e77d3e46
Bartosz Mediger <bartosz@mediger.net>
2019-04-10 10:40:23 +0200

    Path fix

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -48,7 +48,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: "js/[name]",
-        publicPath: '/'
+        publicPath: ''
     },
 
     module: {
